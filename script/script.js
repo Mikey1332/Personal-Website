c2b526045c5bff4998d492e8241a01a6720fd0a8
Michael Kroner <you@example.com>
2024-03-28 08:08:37 -0400

    sidebar updates, about page, projects list, projects, much more

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ let currentPage = 0;
 let pages;
 let pageCountEl = document.getElementById('total-pages');
 let pageNumberEl = document.getElementById('current-page');
+// Get a reference to the element that you want to work with
+var img = document.querySelector("img.img-default");
 
 initializePages();
 
@@ -32,10 +34,38 @@ function prevPage() {
 
 }
 
+// function backPage() {
+//     window.history.back();
+
+// }
+
 function toggleNav() {
     let navbar = document.getElementById('sidebar-nav');
-    navbar.classList.toggle('sidebar-shown');
-    
+    navbar.classList.toggle('sidebar-shown') 
+}
+
+function projectsNavOn() {
+    let navbar = document.getElementById('projects-nav');
+    // navbar.classList.toggle('projects-shown')
+    navbar.classList.add('projects-shown')
+}
+
+function projectsNavOff() {
+    let navbar = document.getElementById('projects-nav');
+    // navbar.classList.toggle('projects-shown')
+    navbar.classList.remove('projects-shown')
 }
 
 
+// Set up an event handler. Notice that we don't use "on" in front
+// of the event name when doing it this way.
+img.addEventListener("mouseover", changeDef);
+
+function changeDef(event){
+  console.log(event.target);
+}
+
+$('#go').click(function(event){
+    var email =  $('mkroner24@pascack.org').val();
+    window.location.href = 'https://mail.google.com/mail/?view=cm&fs=1&to='+email;
+    })
